67a1de8d39a5aa0978ce5aed9908e399ba4df1dd
balaji <balajisathiya12@gmail.com>
2022-09-06 14:28:42 -0500

    Starting parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,16 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  console.log('Input:', json);
+  console.log('Expected:', JSON.parse(json));
+
+  if (json === '[]') {
+    return [];
+  }
+  if (json[0] === '{') {
+    var ind = json.indexOf(':');
+    var key = json.subString(1, ind);
+    var value = json.subString(ind + 1, )
+    console.log(key , value);
+  }
 };
